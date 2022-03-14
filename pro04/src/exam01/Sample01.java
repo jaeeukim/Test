cf4fe468885c37cc8eb56194d5f20f40555806e5
김재은 <bbori530@naver.com>
2022-03-14 09:31:03 +0900

    .

--- pro04/src/exam01/Sample01.java
+++ pro04/src/exam01/Sample01.java
@@ -3,8 +3,27 @@ package exam01;
 public class Sample01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/* 배열
+		 * 
+		 * 배열 선언 방법
+		 * 		자료형[] 배열명;
+		 * 		자료형 배열명[];
+		 * 
+		 * 배열에 값을 할당하기 위한 방법
+		 * 		배열명 = new 자료형[배열크기]; //배열의 크기를 먼저 지정. 
+		 * 		배열명[위치값] = 초기값;  // 배열의 크기가 지정된 후에 원하는 위치(index)에 초기값 할당 
+		 * 
+		 * */		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
