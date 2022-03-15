81a05c404e98507a577492adf95bd7477706f6e7
김재은 <bbori530@naver.com>
2022-03-15 14:46:32 +0900

    배열 예제문제

--- pro04/src/exam02/Sample04.java
+++ pro04/src/exam02/Sample04.java
@@ -12,15 +12,18 @@ public class Sample04 {
 		 */
 		
 		int[][] arr1 = new int[4][4];
-		
+		int num = 1;
 		for(int i = 0; i < arr1.length; i++) {
-			for(int j = 0; j < arr1.length; j++) {
-				arr1[i][j] = 4 * i + j + 1;
+			for(int j = 0; j < arr1[i].length; j++) {
+				arr1[i][j] = num++;
 			}
 		}
 		
 		for(int i = 0; i < arr1.length; i++) {
-			System.out.println(Arrays.toString(arr1[i]));
+			for(int j = 0; j < arr1[i].length; j++) {
+				System.out.print(arr1[i][j] + "\t");				
+			}
+			System.out.println("");
 		}
 		
 		
@@ -34,18 +37,48 @@ public class Sample04 {
 		
 		for(int i = 0; i < arr2.length; i++) {
 			for(int j = 0; j < arr2[i].length; j++) {
-				int num = rand.nextInt(49) + 1;
-				arr2[i][j] = num;
+				arr2[i][j] = rand.nextInt(49) + 1;
 			}
 		}
+		
 		for(int i = 0; i < arr2.length; i++) {
-			System.out.println(Arrays.toString(arr2[i]));
+			for(int j = 0; j < arr2[i].length; j++) {
+				System.out.print(arr2[i][j] + "\t");				
+			}
+			System.out.println("");
 		}
+		
+		System.out.println("===================");
+		
+		
+		
 		
 		/*
 		 * 1 ~ 49 사이의 임의의 정수를 5xN 배열에 순차적으로 저장하고 표 형식으로 출력
 		 * (N의 경우 3 ~ 6 사이의 임의의 크기로 만들어 저장되도록 한다. <- 가변 길이 2차 배열)
 		 */
+		
+		
+	
+		
+		int[][] arr3 = new int[5][];
+		
+		for(int i = 0; i < arr3.length; i++) {
+			arr3[i] = new int[rand.nextInt(4) + 3];
+			for(int j = 0; j < arr3[i].length; j++) {
+				arr3[i][j] = rand.nextInt(49) + 1;
+			}
+			
+		}
+		
+		
+		
+		for(int i = 0; i < arr3.length; i++) {
+			for(int j = 0; j < arr3[i].length; j++) {
+				System.out.print(arr3[i][j] + "\t");				
+			}
+			System.out.println("");
+		}
 		
 		
 		
