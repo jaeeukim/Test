b9653fb9fb760e7ee5fab2c11961cf1165c70e97
김재은 <bbori530@naver.com>
2022-06-16 14:43:55 +0900

    JQuery 객체조작

--- websv_pro09/src/main/webapp/static/js/sample08.js
+++ websv_pro09/src/main/webapp/static/js/sample08.js
@@ -1,3 +1,19 @@
 /**
  * 
  */
+window.onload = function() {
+	// .clone(true) 에 대한 이벤트 핸들러 복제는 JQuery로 연결된
+	// 이벤트 핸들러에 대해서만 적용된다.
+	// 기본 DOM의 addEventListener로 등록된 경우 동작하지 않는다.
+	$(".copy1").bind("mouseover", function(){
+			this.style.color = 'red';		
+	});
+	$(".copy1").bind("mouseout", function(){
+			this.style.color = 'black';		
+	});
+	
+	$("input").bind("input", function(){
+		console.log(value);
+	})
+	
+}
