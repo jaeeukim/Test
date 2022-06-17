75ed5a5e217a6d3f17d3955625cb5f42c7f24cc0
김재은 <bbori530@naver.com>
2022-06-17 19:11:28 +0900

    1차평가 문제..제출

--- fortest/src/main/webapp/test_0617/index.js
+++ fortest/src/main/webapp/test_0617/index.js
@@ -1,13 +1,15 @@
 /**
  * 
  */
- 
+
 function f1() {
 	var id1 = document.getElementById("id1");
 	var req1 = document.getElementById("req1");
 	id1.addEventListener("blur", function() {
-		if(id1.value === "") {
+		if(id1.value == "") {
 			req1.innerHTML = "필수정보입니다.";
+		} else {
+			req1.innerHTML = "";
 		}
 	})
 }
@@ -16,8 +18,10 @@ function f2() {
 	var pw1 = document.getElementById("need-chk");
 	var req2 = document.getElementById("surprise");
 	pw1.addEventListener("blur", function() {
-		if(pw1.value === "") {
+		if(pw1.value == "") {
 			req2.innerHTML = "필수정보입니다.";
+		} else {
+			req2.innerHTML = "";
 		}
 	})	
 }
@@ -26,9 +30,11 @@ function f3() {
 	var pw2 = document.getElementById("pw2");
 	var req3 = document.getElementById("req3");
 	pw2.addEventListener("blur", function() {
-		if(pw2.value === "") {
+		if(pw2.value == "") {
 			req3.innerHTML = "필수정보입니다.";
-		}		
+		} else {
+			req3.innerHTML = "";
+		}
 	})	
 }
  
